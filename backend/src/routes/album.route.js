067d92b6c1ac79ae6c19e461a9b98ac84f05cbda
Dhineshkumar <dhineshkumar.velayudham@gmail.com>
2025-08-30 21:47:36 +0530

    Created Album Routes and controllers

--- backend/src/routes/album.route.js
+++ backend/src/routes/album.route.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { getAlbumById, getAllAlbums } from "../controllers/album.controller.js";
 
 const albumRouter = Router();
+
+albumRouter.get("/",getAllAlbums)
+albumRouter.get("/:albumId",getAlbumById)
 
 export default albumRouter;

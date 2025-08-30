147614d6d575380fa9b4c4fe16d116b7310ac8c5
Dhineshkumar <dhineshkumar.velayudham@gmail.com>
2025-08-30 21:35:42 +0530

    Created admin routes and controllers

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,8 +1,21 @@
 import { Router } from "express";
 import { authCallback } from "../controllers/auth.controller.js";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
+import {
+  checkAdmin,
+  createSong,
+  deleteSong,
+  deleteAlbum,
+  createAlbum,
+} from "../controllers/admin.controller.js";
 
 const adminRouter = Router();
 
-adminRouter.get("/callback", authCallback);
+adminRouter.use(protectRoute, requireAdmin);
+adminRouter.get("/check", checkAdmin);
+adminRouter.get("/songs", createSong);
+adminRouter.get("/song/:id", deleteSong);
+adminRouter.get("/albums", createAlbum);
+adminRouter.get("/album/:id", deleteAlbum);
 
 export default adminRouter;

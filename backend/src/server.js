d889375653d0f7e6f53db5aae931df24b107773f
Dhineshkumar <dhineshkumar.velayudham@gmail.com>
2025-08-30 18:17:31 +0530

    Created basic routes floder sturcture

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,32 @@
 import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./lib/db.js";
+import userRouter from "./routes/user.route.js";
+import cors from "cors";
+import adminRouter from "./routes/admin.route.js";
+import albumRouter from "./routes/album.route.js";
+import songRouter from "./routes/song.route.js";
+import authRouter from "./routes/auth.route.js";
+import statRouter from "./routes/stat.route.js";
 
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
+// Middleware
+app.use(cors());
+app.use(express.json());
+// Routes
+app.use("/api/users", userRouter);
+app.use("/api/admin", adminRouter);
+app.use("/api/album", albumRouter);
+app.use("/api/songs", songRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/stat", statRouter);
+
+// Start server
+app.listen(PORT, () => {
   console.log("Server is running on port : " + PORT);
   connectDB();
 });
